8d72c4a5908d1821a49d9dc66d557c729c07f445
eladiozx <eladiolink@gmail.com>
2020-12-05 12:14:13 -0300

    Mudança no for

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log('Mudança no GITHUB')
 
 console.log('TEST 3.0')
 console.log('===============================================================================')
-for(var i=0;i<20;i++){
+for(var i=0;i<30;i++){
   var str='';
   for(var j=0;j<i;j++){
      str+='*'
